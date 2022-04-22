d496b8ee59afb268d0e2d73121131a960255e1e4
LANDER <lrd698@alumnos.unican.es>
2022-04-22 17:50:39 +0200

    web_performance

--- main.c
+++ main.c
@@ -11,5 +11,6 @@ int main(){
     printf("Cambio main 4\n");
     printf("Correlation of Concepts and Shapes\n");
     printf("Color Palette Selection\n");
+	printf("Web Performance\n");
     return 0;
 }
